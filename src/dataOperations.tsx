891e820931a1a663cc46ef5a96bbb34e4e5859b1
Tal Vin <taltal3032@gmail.com>
2023-10-03 14:09:11 +0300

    Refactor: Moved data operations to another file

--- src/dataOperations.tsx
+++ src/dataOperations.tsx
@@ -1,20 +1,45 @@
 import axios from "axios";
-import React from "react";
+// import React from "react";
 import WeatherApi from "./types/WeatherApi";
+import CoordApi from "./types/CoordApi";
 
-const dataOperations = () => {
-  //   const getWeatherData = async (cityName: string) => {
-  //     try {
-  //       const apiKey = "e58a8b2d3a37b9e6bd3507aa9de5ea2e";
-  //       const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-  //       const res = await axios.get<WeatherApi>(url);
-  //       return res.data;
-  //     } catch (err) {
-  //       console.error("Error:", err);
-  //       throw err;
-  //     }
-  //   };
-  //   return <div></div>;
+const getWeatherData = async (PlaceName: string) => {
+  const tempType = localStorage.getItem("tempType") ?? "metric";
+  try {
+    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${PlaceName}&appid=${weatherApiKey}&units=${tempType}`;
+    let res = await axios.get<WeatherApi>(url);
+    return res.data;
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
 };
 
-export default dataOperations;
+const convertNameToCoord = async (cityName: string) => {
+  if (cityName === "") {
+    console.log("Coordinates Were Not loaded Correctly");
+  } else {
+    try {
+      const coordApiKey = process.env.REACT_APP_AZURE_MAPS_API_KEY;
+      const url = `https://atlas.microsoft.com/search/fuzzy/json?&api-version=1.0&subscription-key=${coordApiKey}&language=en-US&query=${cityName}`;
+      const response = await axios.get<CoordApi>(url);
+      const coordData = response.data;
+      if (coordData.results && coordData.results.length > 0) {
+        const lat = coordData.results[0].position.lat;
+        const lon = coordData.results[0].position.lon;
+        return {
+          lat: lat,
+          lon: lon,
+          municipality: coordData.results[0].address.municipality,
+        };
+      } else {
+        console.log("Coordinates not found");
+      }
+    } catch (error) {
+      console.log("Coordinates Request Error");
+    }
+  }
+};
+
+export { convertNameToCoord, getWeatherData };
--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,69 +1,33 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import WeatherApi from "../types/WeatherApi";
 import { Link, useParams } from "react-router-dom";
 import cloud_icon from "../images/cloud_icon.png";
 import "./LocationPage.css";
 import MyMapComponent from "../components/MapComponent";
-import CoordApi from "../types/CoordApi";
+import { convertNameToCoord } from "../dataOperations";
+import { getWeatherData } from "../dataOperations";
 
 const LocationPage = () => {
   const [weatherData, setWeatherData] = useState<WeatherApi | undefined>(undefined);
   const [coord, setCoord] = useState<[number, number]>([0, 0]);
   const [loading, setLoading] = useState<boolean>(true);
-  const tempType = localStorage.getItem("tempType") ?? "metric";
-  const { cityName } = useParams();
+  let { cityName } = useParams();
 
-  //Move to another file
-  const getWeatherData = async () => {
-    try {
-      const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=${tempType}`;
-      const res = await axios.get<WeatherApi>(url);
-      return res.data;
-    } catch (err) {
-      console.error("Error:", err);
-      throw err;
-    }
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const convertNameToCoord = async (name: string) => {
-    if (name === "") {
-      console.log("Coordinates Were Not loaded Correctly");
-    } else {
-      try {
-        const coordApiKey = process.env.REACT_APP_AZURE_MAPS_API_KEY;
-        const url = `https://atlas.microsoft.com/search/fuzzy/json?&api-version=1.0&subscription-key=${coordApiKey}&language=en-US&query=${cityName}`;
-        const response = await axios.get<CoordApi>(url);
-        const coordData = response.data;
-        if (coordData.results && coordData.results.length > 0) {
-          const lat = coordData.results[0].position.lat;
-          const lon = coordData.results[0].position.lon;
-          setCoord([lat, lon]);
-        } else {
-          console.log("Coordinates not found");
-        }
-      } catch (error) {
-        console.log("Coordinates Request Error");
-      }
-    }
-  };
-
-  //Move to another file
   const fetchData = async () => {
     try {
-      const weatherResponse = await getWeatherData();
+      const coordData = await convertNameToCoord(cityName || "");
+      setCoord((coordinates) => [coordData?.lat || 0, coordData?.lon || 0]);
+      const weatherResponse = await getWeatherData(cityName || "");
       setWeatherData(weatherResponse);
-      await convertNameToCoord(cityName || "");
       setLoading(false);
     } catch (error) {
       window.alert("Error accured, Please try again");
     }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   if (loading) {
     return <h1>Loading...</h1>;
